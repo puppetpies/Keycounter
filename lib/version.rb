215b98568923cccf35501e0bcde23dacfb903800
Bri in The Sky <brianh6854@googlemail.com>
2016-01-29 00:26:31 +0000

    Added version.rb

---
+++ lib/version.rb
@@ -0,0 +1,12 @@
+
+module Keycounter
+
+  module VERSION #:nodoc:
+    MAJOR = 0
+    MINOR = 1
+    TINY = 0
+    CODENAME = "Onion rings!"
+    STRING = [MAJOR, MINOR, TINY].join('.')
+  end
+
+end
